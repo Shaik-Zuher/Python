8a522ea1d8fe6483b8dc8034bb11f2b01635928f
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2024-07-17 00:16:44 +0530

    Add files via upload

---
+++ Programs/Practice/Gcd.py
@@ -0,0 +1,18 @@
+#gcd follow euler theorem
+# if a=b:retun a
+#if a>b return gcd(a-b,b)
+#else return gcd(a,b-a)
+class Solution:
+    def lcmAndGcd(self, A , B):
+        # code here 
+        def gcd(a,b):
+            if a==0:
+                return b
+            if b==0:
+                return a
+            if a==b:
+                return a
+            if a>b:
+                return gcd(a-b,b)
+            return gcd(a,b-a)
+ # lcm can be calculated by lcm=(A*B)//gcd(A,B)
