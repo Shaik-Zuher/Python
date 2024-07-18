488430dfef16d80f751e885c5369dd433c62264c
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2024-07-19 00:10:26 +0530

    Add files via upload

---
+++ Programs/Practice/BS/BS.py
@@ -0,0 +1,39 @@
+#rotated bs 
+#[7,8,9,1,2,3,4,5,6]
+#ie rotated at point to use bs on this 
+#first lets check sorted part and check in that
+#If left part is sorted check if target preesnt in left half or present in right half
+#mid=(s+e)//2
+#how to check which part sorted nums[start]<nums[mid] so left sorted basically ypu can say that(seriously)
+#now how to check if present in sorted part nums[s]<target<nums[mid] so yes then e=mid-1 else s=mid+1
+###effective if elements unique
+#Lets say there are dups
+#[2,8,9,1,2,3,4,5,2]
+#l                r
+#here nums[left]=nums[mid]=nums[right]
+#so then why dont you just change pointers
+#[2,8,9,1,2,3,4,5,2]
+#   l           r
+##################################################
+#IN type of max min problems 
+#always return low but not condition becoz low cntains(min) value but not condition 
+"""
+conditions for bs:
+1)searching
+2)median without sorting
+3)missing
+4)min max elements
+5)min elemrnts
+6)peak
+7)min max dist between gas stations---different casse based on decimal numbers
+"""
+"""
+In question like make array of elements equal minimum moves we can use bs
+in this we use  (lets say cost is outer func to caaculate cost)
+s<=e:
+left=cost(mid)
+right=cost(mid+1)
+if left<right: ############
+e=mid-1
+#########THIS is used in min cost operations type questions
+"""
