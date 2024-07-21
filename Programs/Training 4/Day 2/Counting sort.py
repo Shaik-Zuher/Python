66ff660c797a6b3d626a14e8d5a91f79ad9248ad
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2024-07-22 00:18:54 +0530

    Updated Counting sort.py

--- Programs/Training 4/Day 2/Counting sort.py
+++ Programs/Training 4/Day 2/Counting sort.py
@@ -1,3 +1,5 @@
+######## max->new array len mx+1->store freq->prefix- from freq>use prefix and store
+####It has problems with negative numbers
 
 def performCountingSort(nums):
     n = len(nums)
@@ -16,7 +18,7 @@ def performCountingSort(nums):
     # step-4: calculating prefix sum 
     for index in range(1, mx + 1):
         store[index] += store[index - 1]
-    print(store)
+    print("s",store)
  
  
     # step-5: traverse from right to left and place the element at appropriate index 
@@ -41,4 +43,44 @@ nums = [8, 1, 7, 6, 5, 4, 3, 2, 1, 1, 2, 2, 4, 4, 4, 4, 6]
 print("Before sorting:", nums)
 performCountingSort(nums)
 print("After sorting:", nums)
- 
+##when neg [3,-1]  we usually create array mx+1 [0,0,0,0]
+#                                                0 1 2 3
+#want to store -1 so ill add onw more [0 0 0 0 0] if -2 is there ill add 2 so basically mx+min+1 i.e mx-mn+1 remember min is neg right
+###now about storing
+#lets say i want to store 3  i will have to store next index(due to -1) so basically 3+min=3-(-1)
+#it can be properly accessed this will be offset
+#so for -1 stored in -1-(-1)=0 proper place
+"""
+negative numbers count sort in JS
+var sortArray = function(nums) {
+    let mn=Math.min(...nums);  ----this is changed
+    let off=0;
+    let mx=0
+    if (mn<0){
+     mx=Math.max(...nums)-mn;
+    off=0-mn;--------------------------------------------
+    }
+    else{
+    mx=Math.max(...nums);
+    }
+    let ans=[];
+    let store=[];
+    for(var i=0;i<=mx+1;i++){
+        store.push(0);
+    };
+    for(var i of nums){
+        ans.push(-1);
+        store[i+off]+=1;
+    };
+    let s=0;
+    for(i=0;i<store.length;i++){
+        s+=store[i];
+        store[i]=s;
+    };
+    for(i=nums.length-1;i>-1;i--){
+        store[nums[i]+off]-=1;
+        ans[store[nums[i]+off]]=nums[i];
+    };
+    return ans;
+};
+"""
