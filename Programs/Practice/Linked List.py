2778061b4db086cc7ddd8b5a5f4774cc83e0a860
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2024-07-31 17:30:33 +0530

    Updated Linked List.py

--- Programs/Practice/Linked List.py
+++ Programs/Practice/Linked List.py
@@ -35,3 +35,85 @@ with 1 step and count the nxt time they meet the count is given"""#while hare==t
 """Can also be used to find starting point in loop
 when both meet make turtle =head and incerment both with 1 the nxxt time they meet that is start
 take examples and visuize in mind"""
+
+"""
+138. Copy List with Random Pointer
+A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x and y in the copied list, x.random --> y.
+Return the head of the copied linked list.
+The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+val: an integer representing Node.val
+random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
+Your code will only be given the head of the original linked list.
+Example 1:
+Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+"""
+#this can be solved using hashmap
+#for every node create new node and map it ti original node
+#next time traverse original head and connect next and random to new node created and mapped
+#consider as map:
+#
+#  original node2:new node2
+#  original node:new node    mapping
+#####Or we can create new node and place them in middle of nodes
+#we create new node and place them between original nodes
+#next time we connect the random pointers
+#next we connect their original nexts
+
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        if not head:
+            return head
+        temp=head
+        #creating new nodes between original nodes
+        #7[null]->6[1]->8[0]  randoms[]
+        while temp:
+            new=Node(temp.val)
+            nx=temp.next
+            temp.next=new
+            temp=temp.next
+            temp.next=nx
+            temp=temp.next
+        temp=head
+        nx=temp.next
+        #7[null]->7(c)[]->6[1]->6(c)[]->8[0]->8(c)[]    new nodes placed
+        #first we store random poinetrs in varable valled rd but random points to original nodes but new nodes are next so temp.random.next
+        #it can also be null so we place it in if-else loop
+        #next we connect the random pointers
+        #
+        while temp:
+            if temp.random:
+                rd=temp.random.next
+            else:
+                rd=None
+            #7[null]->7(c)[]->6[1]->6(c)[]->8[0]->8(c)[] rd=null
+            #7[null]->7(c)[null]->6[1]->6(c)[]->8[0]->8(c)[]
+            #7[null]->7(c)[null]->6[1]->6(c)[]->8[0]->8(c)[] rd=1
+            #7[null]->7(c)[null]->6[1]->6(c)[1]->8[0]->8(c)[]
+            temp.next.random=rd#for movings
+            temp=temp.next.next
+        temp=head
+        x=nx
+        #next we set the next pointers
+        #removing links
+        while temp.next and x.next:
+            hnx=temp.next.next
+            nnx=x.next.next
+            temp.next=hnx
+            x.next=nnx
+            temp=temp.next
+            x=x.next
+        temp.next=None
+        return nx
