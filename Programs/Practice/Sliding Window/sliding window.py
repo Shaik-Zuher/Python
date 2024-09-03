cd58f3f5ce0e41d734dccf14f5ff9a504cbe54d2
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2024-09-04 00:08:46 +0530

    Create sliding window.py

---
+++ Programs/Practice/Sliding Window/sliding window.py
@@ -0,0 +1,17 @@
+#sliding always need not to have some array called as window.It can also be achived with pointers(i.e type of 2 pointers)
+#window can be fixed size or any size.This is just a mechanism
+"""
+patterns
+1)fixed window size
+2)longest common substring with condition
+3)no of subarrays with condition
+4)minimum subarray with condition
+"""
+#pattern 4 is not the same as min max sliding window(this is stack)
+"""
+Identify:
+1)some fixed size
+2)Contagious 
+3)subarray,subsequence
+4)consecutive
+"""
