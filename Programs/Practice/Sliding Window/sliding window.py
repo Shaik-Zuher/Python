14abb3960076fbcc0379ad9afb52fad1ade3ac66
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2024-09-05 00:02:49 +0530

    Update sliding window.py

--- Programs/Practice/Sliding Window/sliding window.py
+++ Programs/Practice/Sliding Window/sliding window.py
@@ -15,3 +15,86 @@ Identify:
 3)subarray,subsequence
 4)consecutive
 """
+######################Queue
+######Implementation of sliding window requires queue
+#pop(0) takes more complexity than popleft() so try to avoid it
+"""
+fixed window size example
+1)find subarray of length k which has sum equal to some number
+"""
+from collections import deque
+sum=3
+k=3
+arr=[1,4,2,2,-1,1]
+s=0
+window=deque([])
+for i in range(k):
+    window.append(arr[i])
+    s+=arr[i] 
+if s==sum:
+    print(window)
+for i in range(k,len(arr)):
+    a=window.popleft()
+    window.append(arr[i])
+    s-=a
+    s+=arr[i]
+    if s==sum:
+        print(window)
+"""
+contingous,continous,subarray,subsequenec with some condition
+This requires 2 points where 1 will act as start of window and other as end of window
+Here we won't have fixed window size
+EXAMPLE
+1)find max length of subarray which has sum less than or equal to some value
+  a)Brute force---generating all subarrays
+  b)better      -----if give subarray asked
+  c)best------only useful for ones where something like max/min length is asked not usefull for give the subarray
+"""
+ss=4
+#better
+#take 2 pointers l,r 
+# r        end of window(incrementing)     it involves two things expand,shrink
+# l        start of window                                         r      l
+# 1 4 2 2 -1 1
+l,r=0,0
+length=0
+s=0
+while r<len(arr):
+    s+=arr[r]
+    while s>ss:
+        s-=arr[l]
+        l+=1
+    length=max(length,(r-l+1))#dont forget array is 0 based
+    r+=1
+print(length)
+#You might think it is o(n^2) but l doesn,t run for whole length right so it is o(n)+o(n)
+"""
+best approach
+"""
+####We got extra n becuse of while loop 
+#we can observe we want only max so when i want largest why I need to shrink my window somethimg less why don't I remove while
+#lets say i have  1 2 3 1 4 1and sum=6
+#                 i   j
+#                 i     j     sum>6  my max length is 4 so i will only remove first element becuse why i want to chcek something with length small
+#                   i     j     still >6 so one removed mut my max window length is maintained....
+l,r=0,0
+length=0
+s=0
+while r<len(arr):
+    s+=arr[r]
+    if s>ss:    # this is only change
+        s-=arr[l]
+        l+=1
+    length=max(length,(r-l+1))#dont forget array is 0 based
+    r+=1
+print(length)
+"""template
+while r<len(arr):
+    s+=arr[r]
+    if s>ss:    # condition   this is what changes depending on requirement
+        s-=arr[l]
+        l+=1    #shrink
+    length=max(length,(r-l+1))#dont forget array is 0 based
+    r+=1        #expand
+"""
+#This better approch must be used when subarray is asked to be printed
