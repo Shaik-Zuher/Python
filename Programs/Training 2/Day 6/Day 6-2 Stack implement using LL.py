a38ee5b9c2ef3828c0e88e434e2adb9fa4bbbe10
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2024-11-21 00:03:20 +0530

    Update Day 6-2 Stack implement using LL.py

--- Programs/Training 2/Day 6/Day 6-2 Stack implement using LL.py
+++ Programs/Training 2/Day 6/Day 6-2 Stack implement using LL.py
@@ -2,12 +2,36 @@ class Node:
     def __init__(self,data):
         self.data=data
         self.next=None
-head=Node(10)
-def push(head):
-    def iate(head,data):
-        new=Node(data)
-        head.next=new
+head=None
+def push(head,data):
+    new=Node(data)
+    if head==None:
+        head=new
+        return head 
+    temp=head
+    while temp.next:
+        temp=temp.next
+    temp.next=new
+    return head 
+def pop(head):
+    if head==None:
+        return -1
+    temp=head
+    if not head.next:
+        head=None
         return head
-    
-        
-    
+    while temp.next.next!=None:
+        temp=temp.next
+    temp.next=None
+    return head 
+def display(head):
+    temp=head
+    while temp:
+        print(temp.data,end=",")
+        temp=temp.next
+    return None
+head=push(head,20)
+head=push(head,30)
+display(head)
+head=pop(head)
+display(head)
