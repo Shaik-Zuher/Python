a018c1d4ea00b2a00274fa85b48262e749a7386d
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2024-12-03 00:12:41 +0530

    Update BS-Missing Element.py

--- Programs/Practice/BS/BS-Missing Element.py
+++ Programs/Practice/BS/BS-Missing Element.py
@@ -4,7 +4,7 @@ Return the kth positive integer that is missing from this array.
 
  
 Example 1:
-Input: arr = [2,3,4,7,11], k = 5
+Input: arr = [2,3,4,7,11], k = 5.
 Output: 9
 Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
 
