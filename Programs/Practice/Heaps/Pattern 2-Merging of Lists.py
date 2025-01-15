c71034ff83ba3de4f9920b6afa4bc74e0aa39938
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2025-01-16 00:59:18 +0530

    Update Pattern 2-Merging of Lists.py

--- Programs/Practice/Heaps/Pattern 2-Merging of Lists.py
+++ Programs/Practice/Heaps/Pattern 2-Merging of Lists.py
@@ -96,3 +96,66 @@ class Solution:
         return res
     mat=[[1,2,3],[3,4,5]]
     k=4
+
+################################Another important similar pattern--find pair numbers with min sum
+"""
+373. Find K Pairs with Smallest Sums
+
+You are given two integer arrays nums1 and nums2 sorted in non-decreasing order and an integer k.
+Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+Example 1:
+Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+Output: [[1,2],[1,4],[1,6]]
+Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+Example 2:
+Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+Output: [[1,1],[1,1]]
+Explanation: The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+ 
+Constraints:
+1 <= nums1.length, nums2.length <= 105
+-109 <= nums1[i], nums2[i] <= 109
+nums1 and nums2 both are sorted in non-decreasing order.
+1 <= k <= 104
+k <= nums1.length * nums2.length
+"""
+#k pairs in question tells heap must be used.
+#a=[1,7,11] b=[2,4,6] smallest sum will be first in both arrays 1+2=3
+#Insert into heap pq=[[7(sum),0(index),0(index)]]
+#In above merge problems we take small from pq and increment that particular popped so one removed and one added
+#Here one removed two will be added
+# a=[1,7,11]   a=[1,7,11]
+#      |          |
+# b=[2,4,6]    b=[2,4,6]first value is 1+2 next one will be next will be either (7(index1)+2(index0)) or(1(index0),4(index1)) this is pattern
+#    |              |
+# so intial pq=[[7,0,0]] then next pq=[[5,0,1],[8,1,0]] first array index+1 and second arr index+1
+"""One problem is duplicates"""
+#Let go in order of inserting pq=[[3,0,0]]=>[[5,0,1][8,1,0]]=>[[8,1,0][13,0,2],[11,1,1]]=>[[13,0,2]   [11,1,1][11,1,1]    ,[13,2,0]]
+''''''#                                                                                    see we actually got duplicates
+#To prevent this duplicates use set when adding element to pq to check if index already checked
+class Solution(object):
+    def kSmallestPairs(self, nums1, nums2, k):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        pq=[]
+        ans=[]
+        heappush(pq,[nums1[0]+nums2[0],0,0]) #intial add
+        while len(ans)<k and pq:#Don't forget what i thought you
+            a=heappop(pq)
+            ans.append((nums1[a[1]],nums2[a[2]]))
+            #Don't add index before if condition
+            if a[1]+1<len(nums1):
+                #a[1]+=1     bad move because if you add now other index add(if condition) will be effected
+                new=a[1]+1
+                heappush(pq,[nums1[new]+nums2[a[2]],new,a[2]])
+            if a[2]+1<len(nums2):
+                new=a[2]+1
+                heappush(pq,[nums1[a[1]]+nums2[new],a[1],new])
+        return ans
+#Similar one is sum of max combinations 
