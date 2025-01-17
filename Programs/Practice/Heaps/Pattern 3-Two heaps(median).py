9c240fa3ca4da555570abdf7aabbbbcdb543529b
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2025-01-18 00:39:40 +0530

    Update Pattern 3-Two heaps(median).py

--- Programs/Practice/Heaps/Pattern 3-Two heaps(median).py
+++ Programs/Practice/Heaps/Pattern 3-Two heaps(median).py
@@ -96,3 +96,108 @@ class MedianFinder(object):
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
+'''Sliding Window median'''
+#We can use median in stream pattern here
+"""
+480. Sliding Window Median
+
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle values
+For examples, if arr = [2,3,4], the median is 3.
+For examples, if arr = [1,2,3,4], the median is (2 + 3) / 2 = 2.5.
+You are given an integer array nums and an integer k. There is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+Return the median array for each window in the original array. Answers within 10-5 of the actual value will be accepted.
+
+Example 1:
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]
+Explanation: 
+Window position                Median
+---------------                -----
+[1  3  -1] -3  5  3  6  7        1
+ 1 [3  -1  -3] 5  3  6  7       -1
+ 1  3 [-1  -3  5] 3  6  7       -1
+ 1  3  -1 [-3  5  3] 6  7        3
+ 1  3  -1  -3 [5  3  6] 7        5
+ 1  3  -1  -3  5 [3  6  7]       6
+Example 2:
+Input: nums = [1,2,3,4,2,3,1,4,2], k = 3
+Output: [2.00000,3.00000,3.00000,3.00000,2.00000,3.00000,2.00000]
+ 
+Constraints:
+1 <= k <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+"""
+from heapq import *
+class Solution(object):
+    def __init__(self):
+        self.min=[]
+        self.max=[]
+    def add(self,num):
+        heappush(self.max,-num)
+        if len(self.max)>1+len(self.min):
+            heappush(self.min,-heappop(self.max))
+        if self.max and self.min:
+            if -self.max[0]>self.min[0]:
+                heappush(self.min,-heappop(self.max))
+            if len(self.min)>len(self.max)+1:
+                heappush(self.max,-heappop(self.min))
+    def median(self):
+        if len(self.max)==len(self.min):
+            return (-self.max[0]+self.min[0])/2.0
+        elif len(self.max)>len(self.min):
+            return -self.max[0]
+        else:
+            return self.min[0]
+    def remove(self,num):
+        #print(self.max,self.min,num)
+        if -num in self.max:
+            self.max.remove(-num)
+            heapify(self.max)
+        else:
+            self.min.remove(num)
+            heapify(self.min)
+        #print(self.max,self.min,"l")
+        self.fix()
+        #print(self.max,self.min)
+    def fix(self):
+        if len(self.max)>1+len(self.min):
+            heappush(self.min,-heappop(self.max))
+        if len(self.min)>1+len(self.max):
+            heappush(self.max,-heappop(self.min))
+    def medianSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[float]
+        """
+        ans=[]
+        for i in range(len(nums)):
+            self.add(nums[i])
+            if i>=k-1:
+                ans.append(self.median())
+                self.remove(nums[i-(k-1)])
+        return ans
+##This one gives TLE in python because of remove it takes O(n) as it is happening within another loop of O(n)
+###This problem is mainly in python because in java remove is O(logn) and we also have treeset in Java
+####For python we have sortedList method which is sorted array but the opertions are like set there are no append,pop whenever element is added it automaticaly is sorted the same with removing element
+####Main point all these operations happen in O(logn) -----but not suitable for interviews but learn it if lucky useful
+from sortedcontainers import SortedList
+class Solution(object):
+    def medianSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[float]
+        """
+        def median(s):
+            if len(s)%2==1:
+                return s[len(s)//2]
+            return (s[len(s)//2]+s[len(s)//2-1])/2.0
+        ans=[]
+        l=SortedList()
+        for i in range(len(nums)):
+            l.add(nums[i])
+            if(i>=k-1):
+                ans.append(median(l))
+                l.discard(nums[i-(k-1)])
+        return ans
