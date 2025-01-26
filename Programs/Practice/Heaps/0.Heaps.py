4dd4931419053944492f07083c8c05bfe3ae68f9
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2025-01-27 01:16:33 +0530

    Update 0.Heaps.py

--- Programs/Practice/Heaps/0.Heaps.py
+++ Programs/Practice/Heaps/0.Heaps.py
@@ -1,5 +1,5 @@
 #Heaps can be many sometimes tree or queue
-#Heap is tree like structure in which root node is either most maximum or most mimimum
+#Heap is tree like structure in which root node is either most maximum or most mimimum.
 #To help the coder it is made to look like priority queue(abstraction)
 #heaps always need not to be numbers they can can be lists
 #[[0,"a"],[1,"b"],[2,"c"]] like this
