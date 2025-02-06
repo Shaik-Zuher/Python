68e9561d1fb439f49d98eb1157ee915e8d3aad65
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2025-02-07 00:24:14 +0530

    Update Design Twitter.py

--- Programs/Practice/Heaps/Design Twitter.py
+++ Programs/Practice/Heaps/Design Twitter.py
@@ -1,4 +1,4 @@
-#This is pattern of merge k sorted lists
+#This is pattern of merge k sorted lists.
 """
 Design a simplified version of Twitter where users can post tweets, follow/unfollow another user, and is able to see the 10 most recent tweets in the user's news feed.
 Implement the Twitter class:
