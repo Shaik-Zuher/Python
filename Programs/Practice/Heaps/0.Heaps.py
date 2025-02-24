6056ea1249ed881f610be7d904e6cf9a22237d0d
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2025-02-25 00:16:01 +0530

    Update 0.Heaps.py

--- Programs/Practice/Heaps/0.Heaps.py
+++ Programs/Practice/Heaps/0.Heaps.py
@@ -1,16 +1,16 @@
-#Heaps can be many sometimes tree or queue.
-#Heap is tree like structure in which root node is either most maximum or most mimimum.
-#To help the coder it is made to look like priority queue(abstraction).
-#heaps always need not to be numbers they can can be lists.
-#[[0,"a"],[1,"b"],[2,"c"]] like this.
-#[[0,2],[0,1]] when min heap smallest number at index 0 will be popped if tied then second element is checked.
-#########Heap is not sorted at all.
+#Heaps can be many sometimes tree or queue
+#Heap is tree like structure in which root node is either most maximum or most mimimum
+#To help the coder it is made to look like priority queue(abstraction)
+#heaps always need not to be numbers they can can be lists
+#[[0,"a"],[1,"b"],[2,"c"]] like this
+#[[0,2],[0,1]] when min heap smallest number at index 0 will be popped if tied then second element is checked
+#########Heap is not sorted at all
 """
 Identification:
-Min or max element,K.
+Min or max element,K
 It requires sorting
 #There is confusion with binary search(same minumum type problems)
-#Both can be used Time of bs is nlogn(if sort needed) and Time of heap is nlogk.
+#Both can be used Time of bs is nlogn(if sort needed) and Time of heap is nlogk
 ######Most important-Whenever we constantly need to sort array after inserting or deleting element and get some largest or smallest elements
       then this heap can be used becoz heap is always sorted even if you insert a new element or delete element
 
@@ -78,3 +78,139 @@ def ksmall(arr,k):
         if len(ans)>k:
             heappop(ans)
     return heappop(ans)*-1
+
+#We have problem called as skyline problem can be solved by both heap,line sweep need to get intution
+"""
+218. The Skyline Problem
+
+A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Given the locations and heights of all the buildings, return the skyline formed by these buildings collectively.
+The geometric information of each building is given in the array buildings where buildings[i] = [lefti, righti, heighti]:
+lefti is the x coordinate of the left edge of the ith building.
+righti is the x coordinate of the right edge of the ith building.
+heighti is the height of the ith building.
+You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
+The skyline should be represented as a list of "key points" sorted by their x-coordinate in the form [[x1,y1],[x2,y2],...]. Each key point is the left endpoint of some horizontal segment in the skyline except the last point in the list, which always has a y-coordinate 0 and is used to mark the skyline's termination where the rightmost building ends. Any ground between the leftmost and rightmost buildings should be part of the skyline's contour.
+Note: There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [...,[2 3],[4 5],[7 5],[11 5],[12 7],...] is not acceptable; the three lines of height 5 should be merged into one in the final output as such: [...,[2 3],[4 5],[12 7],...]
+
+Example 1:                     need to return outlines of buildings
+15|    |11|
+  |    |11|
+  |    |1|33333|
+10|   |00000|33|     |4444|
+  |   |00000|33|     |4444|
+  |   |00000|33|     |444|555555|  
+5 |   |00000|33|     |444|555555|
+  |   |00000|33|     |444|555555|
+0 |   |00000|33|     |444|555555|
+   _________________________________
+   0     5    10    15    20     25 
+15|    x  |
+  |    |  |                           return x marks
+  |    | |X    |
+10|   X     |  |     X    |
+  |   |     |  |     |    |
+  |   |     |  |     |   |X     |  
+5 |   |     |  |     |   |      |
+  |   |     |  |     |   |      |
+0 |   |     |  X     |   |      |
+   _________________________________
+   0     5    10    15    20     25 
+Input: buildings = [[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]
+Output: [[2,10],[3,15],[7,12],[12,0],[15,10],[20,8],[24,0]]
+Explanation:
+Figure A shows the buildings of the input.
+Figure B shows the skyline formed by those buildings. The red points in figure B represent the key points in the output list.
+Example 2:
+Input: buildings = [[0,2,3],[2,5,3]]
+Output: [[0,3],[5,0]]
+ 
+Constraints:
+1 <= buildings.length <= 104
+0 <= lefti < righti <= 231 - 1
+1 <= heighti <= 231 - 1
+buildings is sorted by lefti in non-decreasing order.
+"""
+#brutely observe how we can solve this  
+#answer contains x-axis(point) and height--so i need this
+#Intitution is use max heap and keep track of height whenver height changes then that is one of answer
+#Now how to check heights i mean at point it is added and another point removed so track it
+#we can run loop from 0 to max(end buliding  point)-Might be TLE haven't tried
+###another point i only need to check building start and end indices right so about sorted(set(start,end of all builidng))
+class Solution(object):
+    def getSkyline(self, buildings):
+        """
+        :type buildings: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        sp={}#start points
+        ep={}#end points
+        ans=[]
+        pq=[]
+        points=set()
+        for i in buildings:
+            if i[0] not in sp:
+                sp[i[0]]=[i[2]]
+            else:
+                sp[i[0]].append(i[2])
+            if i[1] not in ep:
+                ep[i[1]]=[i[2]]
+            else:
+                ep[i[1]].append(i[2])
+            points.add(i[1])
+            points.add(i[0])
+        points=list(points)
+        points.sort()
+        lazy={}#lazy removal
+        heappush(pq,0)#just for checking last point in answer
+        #There are 3 cases 1)if start point 
+        #                  2)if end point
+        #                  3)If both start and end then priority is end first
+        for i in points:
+            if i in sp and i in ep:#case3
+                while -pq[0] in lazy:
+                    lazy[-pq[0]]-=1
+                    if lazy[-pq[0]]==0:
+                        del lazy[-pq[0]]
+                    heappop(pq)
+                for j in ep[i]:
+                    if j in lazy:
+                        lazy[j]+=1
+                    else:
+                        lazy[j]=1
+                a=pq[0]
+                while -pq[0] in lazy:
+                    lazy[-pq[0]]-=1
+                    if lazy[-pq[0]]==0:
+                        del lazy[-pq[0]]
+                    heappop(pq)
+                for j in sp[i]:
+                    heappush(pq,-j)
+                if(a!=pq[0]):#check when removal also
+                    ans.append([i,-pq[0]])
+                
+            elif i in sp:#case1
+                a=pq[0]
+                for j in sp[i]:
+                     heappush(pq,-j)
+                if(a!=pq[0]):
+                    ans.append([i,-pq[0]])
+            else:#case 2
+                while -pq[0] in lazy:
+                    lazy[-pq[0]]-=1
+                    if lazy[-pq[0]]==0:
+                        del lazy[-pq[0]]
+                    heappop(pq)
+                for j in ep[i]:
+                    if j in lazy:
+                        lazy[j]+=1
+                    else:
+                        lazy[j]=1
+                a=-pq[0]
+                while -pq[0] in lazy:
+                    lazy[-pq[0]]-=1
+                    if lazy[-pq[0]]==0:
+                        del lazy[-pq[0]]
+                    heappop(pq)
+                if(a!=-pq[0]):#check when removal also
+                    ans.append([i,-pq[0]])
+        return ans
