efcdad9e8e21a79a9113a34219286b2e6bb5b4c4
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2025-03-20 05:38:42 +0530

    Update Greedy.py

--- Programs/Practice/Greedy.py
+++ Programs/Practice/Greedy.py
@@ -1,6 +1,6 @@
 #Greedy actually subtopic of dp and even sometimes heap.
 #Greedy involves the point of becoming really greedy like man what does it even min.
-#Greedy identified by optimal,maximum,minimum,smallest.largest.
+#Greedy identified by optimal,maximum,minimum,smallest,largest.
 #Like i always say minimum can be bs,heap,greedy,dp.
 """
 Tip: When ever minimum is given in question first see if there is any monotoic nature(BS).
